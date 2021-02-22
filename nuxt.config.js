4fb53f8203c5ee4de195e0b113863a4d36ad1b86
David de Boer <d.deboer@parkos.com>
2021-02-22 14:25:22 +0100

    Add GAE runtime config

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,5 +49,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  server: {
+    port: 3000,
+    host: '127.0.0.1',
+    timing: false
   }
 }
