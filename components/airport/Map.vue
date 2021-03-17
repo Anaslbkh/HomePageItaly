78815ad14e681f8881c41aed8a5931b4fe814818
David de Boer <d.deboer@parkos.com>
2021-03-17 15:30:46 +0100

    Fix styling

--- components/airport/Map.vue
+++ components/airport/Map.vue
@@ -5,21 +5,22 @@
 </template>
 
 <script lang="ts">
-import { Loader } from "@googlemaps/js-api-loader"
+import { Loader } from '@googlemaps/js-api-loader'
 import Vue from 'vue'
 
 if (process.client) {
   const loader = new Loader({
     apiKey: process.env.GOOGLE_MAPS_API_KEY || '',
-    version: "3.44"
-  });
+    version: '3.44'
+  })
 
   loader.load().then(() => {
-    var map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    /* global google */
+    return new google.maps.Map(document.getElementById('map') as HTMLElement, {
       center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
-  });
+      zoom: 8
+    })
+  })
 }
 
 export default Vue.extend({
