4c90aa7af1c01fde0d95a35d15e651292afceb70
David de Boer <d.deboer@parkos.com>
2021-03-30 17:20:10 +0200

    Change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { name: 'msapplication-TileConfig', content: 'https://assets.parkos.com/assets/favicon/browserconfig.xml' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: 'https://assets.parkos.com/assets/favicon/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com/' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bree+Serif&family=Roboto:wght@400;700&display=swap&family=Material+Icons' },
       { rel: 'apple-touch-icon', href: 'https://assets.parkos.com/assets/favicon/apple-icon.png' },
