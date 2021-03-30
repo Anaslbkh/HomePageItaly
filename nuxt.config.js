388f5156d81a076f78dc2a98c64ca454ffc04ad2
David de Boer <d.deboer@parkos.com>
2021-03-30 17:44:17 +0200

    Always load i18n and domain plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,9 +44,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '@/plugins/i18n.ts', mode: process.env.NODE_ENV === 'production' ? 'server' : 'all' },
+    { src: '@/plugins/i18n.ts', mode: 'all' }, // mode: process.env.NODE_ENV === 'production' ? 'server' : 'all' }, @TODO review/move function server side
     '@/plugins/axios',
-    { src: '@/plugins/domain.ts', mode: process.env.NODE_ENV === 'production' ? 'server' : 'all' },
+    { src: '@/plugins/domain.ts', mode: 'all' }, // mode: process.env.NODE_ENV === 'production' ? 'server' : 'all' }, @TODO review/move function server side
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
