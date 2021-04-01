b4fb97755756b68434fac6783478f88e64af3e53
David de Boer <d.deboer@parkos.com>
2021-04-01 10:30:42 +0200

    Hide empty price

--- components/airport/Parking.vue
+++ components/airport/Parking.vue
@@ -9,7 +9,9 @@
       </div>
       <p class="flex flex-col text-center p-4 border-t border-gray-500">
         <strong>{{ parking.name }}</strong>
-        <span>{{ $i18n('templates.from-x-euro-week', { amount: price }) }}</span>
+        <span v-if="this.parking.from_price">
+          {{ $i18n('templates.from-x-euro-week', { amount: price }) }}
+        </span>
       </p>
     </article>
   </a>
