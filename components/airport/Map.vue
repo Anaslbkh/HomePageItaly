5a08e410bcb1b342aa5a6490735f3c9f1bd7bb7f
David de Boer <d.deboer@parkos.com>
2021-04-07 18:10:13 +0200

    Add loading of map when in viewport

--- components/airport/Map.vue
+++ components/airport/Map.vue
@@ -26,36 +26,73 @@ export default Vue.extend({
     },
   },
 
-  mounted(): void {
-    const { latitude, longitude } = this.$currentAirport.address
-    const coordinatesList: Array<Pick<AddressType, 'latitude' | 'longitude'>> = this.coordinates
-    const loader = new Loader({
-      apiKey: process.env.GOOGLE_MAPS_API_KEY || '',
-      version: '3.44'
-    })
+  data(): {
+    mapWasInViewPort: boolean,
+  } {
+    return {
+      mapWasInViewPort: false,
+    }
+  },
 
-    loader.load().then(() => {
-      const bounds = new google.maps.LatLngBounds()
-      const map = new google.maps.Map(document.getElementById('google-maps') as HTMLElement, {
-        center: new google.maps.LatLng(latitude, longitude),
-        zoom: 12
-      })
+  methods: {
+    loadMapIfVisible(): void {
+      // @ts-ignore
+      const mapRect = document.getElementById('google-maps').getBoundingClientRect()
 
-      const icon = require('~/assets/airport/maps/marker.svg')
+      if (
+        this.mapWasInViewPort === false &&
+        mapRect.top >= 0 &&
+        mapRect.top <= (window.innerHeight || document.documentElement.clientHeight)
+      ) {
+        this.mapWasInViewPort = true
 
-      coordinatesList.forEach(({ latitude, longitude }: Pick<AddressType, 'latitude' | 'longitude'>) => {
-        const marker = new google.maps.Marker({
-          icon,
-          position: new google.maps.LatLng(latitude, longitude),
-          map
+        const { latitude, longitude } = this.$currentAirport.address
+        const coordinatesList: Array<Pick<AddressType, 'latitude' | 'longitude'>> = this.coordinates
+        const loader = new Loader({
+          apiKey: process.env.GOOGLE_MAPS_API_KEY || '',
+          version: '3.44'
         })
-        // @ts-ignore
-        bounds.extend(marker.getPosition())
-      })
 
-      map.fitBounds(bounds)
-      map.setCenter(bounds.getCenter())
-    })
+        loader.load().then(() => {
+          const bounds = new google.maps.LatLngBounds()
+          const map = new google.maps.Map(document.getElementById('google-maps') as HTMLElement, {
+            center: new google.maps.LatLng(latitude, longitude),
+            zoom: 12
+          })
+
+          const icon = require('~/assets/airport/maps/marker.svg')
+
+          coordinatesList.forEach(({ latitude, longitude }: Pick<AddressType, 'latitude' | 'longitude'>) => {
+            const marker = new google.maps.Marker({
+              icon,
+              position: new google.maps.LatLng(latitude, longitude),
+              map
+            })
+            // @ts-ignore
+            bounds.extend(marker.getPosition())
+          })
+
+          map.fitBounds(bounds)
+          map.setCenter(bounds.getCenter())
+        })
+      }
+    }
+  },
+
+  created(): void {
+    if (process.client) {
+      window.addEventListener('scroll', this.loadMapIfVisible);
+    }
+  },
+
+  destroyed(): void {
+    if (process.client) {
+      window.removeEventListener('scroll', this.loadMapIfVisible);
+    }
+  },
+
+  mounted(): void {
+
   }
 })
 </script>
