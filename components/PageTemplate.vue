e5c9d8d438bb206c1f51f60c542eb42cbe2b048b
abdullahevo <abdullah.evoo@gmail.com>
2021-09-23 13:06:10 +0200

    bff proxy init

--- components/PageTemplate.vue
+++ components/PageTemplate.vue
@@ -1,46 +1,44 @@
 <template>
-    <div v-if="block" v-html="blockContent" />
+  <div v-if="!$fetchState.pending && block">
+    <div v-html="blockContent"/>
+  </div>
 </template>
 
 <script>
-import { getInstance } from '~/services/apiService'
+  import {getInstance} from '~/services/apiService'
 
-export default {
-  data() {
-    return {
-      block: undefined,
-    }
-  },
+  export default {
+    data() {
+      return {
+        block: undefined,
+      }
+    },
 
-  props: {
-    slug: undefined,
-    language: undefined,
-  },
+    props: {
+      slug: undefined,
+      language: undefined,
+    },
 
-  async fetch() {
-    if (process.server) {
+    async fetch() {
       const api = getInstance('parkos')
+      this.block = await api.getPageTemplate(this.slug, this.language.lang);
+    },
 
-      const block = await api.getPageTemplate(this.slug, this.language.lang);
-      this.block = block;
-    }
-  },
-
-  computed: {
-    blockContent() {
-      return this.block['page_content']
+    computed: {
+      blockContent() {
+        return this.block['page_content']
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-.text-white a,
-.text-white a:hover {
-  color: inherit;
-}
+  .text-white a,
+  .text-white a:hover {
+    color: inherit;
+  }
 
-.strong {
-  font-weight: bolder;
-}
+  .strong {
+    font-weight: bolder;
+  }
 </style>
